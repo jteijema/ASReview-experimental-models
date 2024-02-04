26f36b45c98db3dfdf768d8d2b8ee1603b6b42ee
JT <j.j.teijema@uu.nl>
2024-02-04 12:21:44 +0100

    Update knn.py

--- asreviewcontrib/models/knn.py
+++ asreviewcontrib/models/knn.py
@@ -16,20 +16,15 @@ class KNN(BaseTrainClassifier):
     def __init__(self):
         super().__init__()
         self._is_fit = False
-        disable_logging()
-    @ property
-    def model(self):
-        if not hasattr(self, "_model"):
-            self._model = KNeighborsClassifier()
-        return self._model
+        self._model = KNeighborsClassifier()
 
     def fit(self, X, y):
         if X.shape[0] > 5:
             self._is_fit = True
-            return self.model.fit(X, y)
+            return self._model.fit(X, y)
 
     def predict_proba(self, X):
         if self._is_fit:
-            return self.model.predict_proba(X)
+            return self._model.predict_proba(X)
         else:
             return np.zeros((X.shape[0], 2))
